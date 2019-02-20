e88fd2411e89d731de6458677fd87b08a64243bc
Przemek Malon <przemekmalon@hotmail.co.uk>
2019-02-20 02:40:38 +0000

    Fix POST listen function. Now receives state correctly

--- RaspberryPi/src/server/typeswipe_server.js
+++ RaspberryPi/src/server/typeswipe_server.js
@@ -6,9 +6,11 @@ const port = 8080;
 
 app.get('/', (req, res) => res.send('TypeSwipe raspberry pi here. Send a post request with { \'state\' : [on/off] } to this server to control the robot state'));
 
-app.post('/home/student/', function(req, res) {
-	var msg = req.params.state;
-	console.log('Received post message. State param = ' + msg);
+app.post('/', function(req, res) {
+	var msg = req.query.state;
+	var response = 'Received post message. State param = ' + msg;
+	console.log(response);
+	res.send(response);
 });
 
 app.listen(port, () => console.log('Server listening on port ${port}'));
