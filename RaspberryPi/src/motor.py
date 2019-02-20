bdb42466e207d7171ddac461aa9ff8614c493186
Seanmullan <seanmullan97@outlook.com>
2019-02-20 17:49:18 +0000

    Refactor motor class

--- RaspberryPi/src/motor.py
+++ RaspberryPi/src/motor.py
@@ -4,40 +4,44 @@
 - author ****
 '''
 
+#pylint: disable=import-error
 import smbus
 
-bus = smbus.SMBus(1)
-address = 0x04
-movement = ''
+BUS = smbus.SMBus(1)
+ADDRESS = 0x04
+MOVEMENT = ''
 #---------------------------------------------------------------------------
 #                              Useful functions
 #---------------------------------------------------------------------------
 
 # Writing to motorboard
-def motor_move(id, power):
+def motor_move(motor_id, power):
     """
     Smooth control of the motors
     Mode 2 is Forward.
     Mode 3 is Backwards.
     """
     mode = 2 if power >= 0 else 3
-    cmd_byte = id << 5 | 24 | mode << 1
+    cmd_byte = motor_id << 5 | 24 | mode << 1
     pwr = int(abs(power) * 2.55)
-    bus.write_i2c_block_data(address, 0, [cmd_byte, pwr])
+    BUS.write_i2c_block_data(ADDRESS, 0, [cmd_byte, pwr])
 
 def write(value):
-    bus.write_byte_data(address, 0x00, value)
+    """
+    Write data to bus
+    """
+    BUS.write_byte_data(ADDRESS, 0x00, value)
 
-def stopMotor(id):
+def stop_motor(motor_id):
     """
     Mode 0 floats the motor.
     """
     direction = 0
-    byte1 = id << 5 | 16 | direction << 1
+    byte1 = motor_id << 5 | 16 | direction << 1
     write(byte1)
 
 
-def stopMotors():
+def stop_motors():
     """
     The motor board stops all motors if bit 0 is high.
     """
