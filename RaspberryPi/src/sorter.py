22ba2486eb240a2c3604707a70fac295875513fb
Sean Mullan <seanmullan97@outlook.com>
2019-02-22 18:09:26 +0000

    Add sorter structure

---
+++ RaspberryPi/src/sorter.py
@@ -0,0 +1,19 @@
+import threading
+import data
+
+class Sorter(threading.Thread):
+
+    def __init__(self):
+        self.data = data.Data()
+        self.current_classification = ""
+
+    def run(self):
+        while self.data.get_run_system():
+            current_class = self.data.dequeue_classified_queue()
+            self.sort_object(current_class)
+            # TODO: Wait for some period of time or check weight/light sensors
+            # before getting next class
+
+    def sort_object(self, current_class):
+        # Do stuff with motors
+        pass
