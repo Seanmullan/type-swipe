06cbfb96c5409ece50e6d2b972846bb3613ce084
Sean Mullan <seanmullan97@outlook.com>
2019-03-07 18:11:48 +0000

    Add safe thread shut down

--- RaspberryPi/src/toddler.py
+++ RaspberryPi/src/toddler.py
@@ -5,10 +5,12 @@ Entry point for the program, which is called by Sandbox
 import sys
 sys.path.append('data/')
 sys.path.append('motor_control/')
-#pylint: disable=wrong-import-position
 import threading
+import time
 import json
 import data
+import motor
+import serial
 sys.path.append('vision/')
 import preprocessor
 # If the self test is running, import fake conveyor class
@@ -26,33 +28,49 @@ class Toddler(object):
 
     def __init__(self, io):
 
+        # Define sensor thresholds
+        self.proximity_thresh = 13
+        self.inductive_thresh = 300
+
+        # Initialise sensor values
+        self.proximity = 15
+        self.inductive = 100
+
         # Initialise system objects and driver functions
-        self.preprocessor = preprocessor.Preprocessor()
         self.conveyor = conveyor.Conveyor()
         self.data = data.Data()
+        self.preprocessor = preprocessor.Preprocessor()
         self.camera = io.camera.initCamera('pi', 'low')
         self.get_inputs = io.interface_kit.getInputs
         self.get_sensors = io.interface_kit.getSensors
-        self.__control_stop_event = threading.Event()   #Needed for getting stuck in while loops
-        self.__thread_check_run_system_stop_event = threading.Event()
 
-        self.thread_check_run_system = threading.Thread(name="check_run_system", target=self.check_run_system)
+        # Initialise helper threads
+        self.thread_proxi = threading.Thread(name="get_proxi", target=self.get_proxi)
+        self.thread_check_run_system = threading.Thread(
+            name="check_run_system",
+            target=self.check_run_system
+        )
+
+        # Initialise thread stop flags
+        self.control_event = threading.Event()
+        self.check_run_system_event = threading.Event()
 
         # Start system threads
         self.thread_check_run_system.start()
+        self.thread_proxi.start()
         self.preprocessor.start()
 
     def stop_check_run_system(self):
         """
         Sets the stop flag for the check_run_system thread
         """
-        self.__thread_check_run_system_stop_event.set()
+        self.check_run_system_event.set()
 
     def stopped_check_run_system(self):
         """
         Returns true if the check_run_system flag has been set
         """
-        return self.__thread_check_run_system_stop_event.is_set()
+        return self.check_run_system_event.is_set()
 
     def check_run_system(self):
         """
@@ -69,31 +87,25 @@ class Toddler(object):
                 # If system is running, update conveyor belt speed
                 if run_system:
                     self.conveyor.set_belt_speed(conveyor_speed)
+                    motor.motor_move(2, 100)
+                    time.sleep(0.2)
                 else:
                     self.conveyor.stop_belt()
 
                 # Set system wide flag for start/stop
                 self.data.set_run_system(run_system)
 
-
-    """
-    We need a thread stopper event inside the toddler due to
-    The thread getting stuck in the while loops for when an object is passing through
-    As of now we can stop the thread but perhaps it would be useful to improve the
-    control algorithm so that it doesn't get stuck like this
-    """
-
     def stop_control(self):
         """
         Sets the stop flag for the toddler control thread
         """
-        self.__control_stop_event.set()
+        self.control_event.set()
 
     def stopped_control(self):
         """
         Returns true if the control stop flag is set
         """
-        return self.__control_stop_event.is_set()
+        return self.control_event.is_set()
 
     def control(self):
         """
@@ -101,44 +113,81 @@ class Toddler(object):
         whether it is metallic or non-metallic. A '1' is added to the metal queue if metallic, and
         a 0 is added otherwise. While the current object is present, the function will spin until
         the object leaves the sensor zone.
-        TODO: Figure out sensor thresholds
         """
-        #print '{}\t{}'.format(self.get_sensors(), self.get_inputs())
-        sensor_data = self.get_sensors()
-        proximity = sensor_data[0]
-        inductive = sensor_data[1]
+        print '{}\t{}'.format(self.get_sensors(), self.get_inputs())
+        proximity = self.data.get_proximity()
+        inductive = float(self.get_sensors()[2])
 
-        if proximity >= 15:
+        # No object present in sensor zone
+        if proximity >= self.proximity_thresh:
             print 'No object present'
-            while self.get_sensors()[0] >= 15:
-                if self.stopped_control():
-                    break
-                else:
-                    continue
+            self.wait(False)
 
-        elif proximity < 15 and inductive >= 900:
+        # Non-metallic object present in sensor zone
+        elif proximity < self.proximity_thresh and inductive <= self.inductive_thresh:
             self.data.enqueue_metal_queue(0)
             print 'Non-metal object detected'
-            while self.get_sensors()[0] < 15:
-                if self.stopped_control():
-                    break
-                else:
-                    continue
 
-        elif proximity < 15 and inductive < 900:
+        # Metallic object present in sensor zone
+        elif proximity < self.proximity_thresh and inductive > self.inductive_thresh:
             self.data.enqueue_metal_queue(1)
             print 'Metallic object detected'
-            while self.get_sensors()[0] < 15:
+            self.wait(True)
+
+    def wait(self, object_present):
+        """
+        If no object is present, the thread spins until an object is present. If an
+        object is present, the thread spins until the object leaves the sensor zone.
+        """
+        if not object_present:
+            while self.data.get_proximity() >= self.proximity_thresh:
+                print '{}\t{}'.format(self.get_sensors(), self.get_inputs())
                 if self.stopped_control():
                     break
                 else:
-                    continue
+                    time.sleep(0.1)
+        else:
+            while self.data.get_proximity() < self.proximity_thresh:
+                if self.stopped_control():
+                    break
+                else:
+                    time.sleep(0.1)
+        time.sleep(0.2)
 
+    def get_proxi(self):
+        """
+        Reads proximity value from USB port.
+        """
+        ser = serial.Serial('/dev/ttyACM0', 9600)
+        while self.data.get_run_system():
+            if ser.readline() != None:
+                try:
+                    proxi = int(float(ser.readline()))
+                    self.data.set_proximity(proxi)
+                except ValueError:
+                    print "Failed to convert proxi string to int"
 
     def vision(self):
         """
         Called by Sandbox thread. Updates image data in Data class.
         """
-        # image = self.camera.getFrame()
-        # self.data.set_image_raw(image)
-        # self.camera.imshow('Camera', image)
+        image = self.camera.getFrame()
+        self.data.set_image_raw(image)
+        self.camera.imshow('Camera', image)
+        # cap0 = cv2.VideoCapture(1)
+        # ret0, frame0 = cap0.read()
+        # object_name = 'tester'
+        # cv2.imwrite('/home/student/images/' + object_name + str(self.counter) + '.png', frame0)
+        # self.counter += 1
+        # print(frame0.shape)
+        time.sleep(0.05)
+
+    def destroy(self):
+        """
+        Shuts down system threads safely.
+        """
+        print "Shutting down system"
+        self.data.set_run_system(False)
+        self.stop_control()
+        self.stop_check_run_system()
+        self.conveyor.stop_belt()
